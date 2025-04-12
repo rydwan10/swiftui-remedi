f7e575f97839016665df60c112a19f40d6f52c15
Muhammad Rydwan <rydwan.dev@gmail.com>
2025-04-12 20:20:08 +0700

    Initial Commit

---
+++ ReMedi/ReMediApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReMediApp.swift
+//  ReMedi
+//
+//  Created by Muhammad Rydwan on 12/04/25.
+//
+
+import SwiftUI
+
+@main
+struct ReMediApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReMediTests/ReMediTests.swift
@@ -0,0 +1,17 @@
+//
+//  ReMediTests.swift
+//  ReMediTests
+//
+//  Created by Muhammad Rydwan on 12/04/25.
+//
+
+import Testing
+@testable import ReMedi
+
+struct ReMediTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
